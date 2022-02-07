a901374d60764d2863dda2c0c6297d844f696b3a
Yoon <sjallk@gmail.com>
2022-02-07 12:37:24 +0900

    Fix: setting router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,  
+  Route  
+} from 'react-router-dom';
+import Home from './routes/Home'
+import Library from './routes/Library'
+import Component from './routes/Component'
+import Rule from './routes/Rule'
+import NotFound from './routes/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path='/library' element={ <Library /> } />
+        <Route path='/rule' element={ <Rule /> } />
+        <Route path='/component' element={ <Component /> } />
+        <Route path='*' element={ <NotFound /> } />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/routes/Rule.js
@@ -0,0 +1,5 @@
+function Rule() {
+  return <div>Rule</div>
+}
+
+export default Rule;
